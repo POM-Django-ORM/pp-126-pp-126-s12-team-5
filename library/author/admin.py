b79f3796d6f2607f99c154c839f4f83e7191a493
sanya1213 <165886127+sanya1213@users.noreply.github.com>
2025-03-26 09:26:03 +0200

    Update admin.py

--- library/author/admin.py
+++ library/author/admin.py
@@ -1,1 +1,8 @@
 # Register your models here.
+from django.contrib import admin
+from .models import Author
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'birth_date', 'biography')
+    search_fields = ('first_name', 'last_name')
