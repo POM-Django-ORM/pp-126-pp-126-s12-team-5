f2e4e79a6e4f4aa19bfdb66a3195112960730145
sanya1213 <165886127+sanya1213@users.noreply.github.com>
2025-03-26 09:25:16 +0200

    Update 0001_initial.py

--- library/author/migrations/0001_initial.py
+++ library/author/migrations/0001_initial.py
@@ -14,6 +14,10 @@ class Migration(migrations.Migration):
             name='Author',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('first_name', models.CharField(max_length=255)),  # Ім'я автора
+                ('last_name', models.CharField(max_length=255)),   # Прізвище автора
+                ('birth_date', models.DateField(null=True, blank=True)),  # Дата народження автора (необов'язково)
+                ('biography', models.TextField(null=True, blank=True)),  # Біографія автора
             ],
         ),
     ]
