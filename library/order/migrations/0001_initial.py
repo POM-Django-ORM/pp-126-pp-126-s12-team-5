d86c3b13775cf6361371aa9c491a4dca6555c891
sanya1213 <165886127+sanya1213@users.noreply.github.com>
2025-03-26 09:22:08 +0200

    Update 0001_initial.py

--- library/order/migrations/0001_initial.py
+++ library/order/migrations/0001_initial.py
@@ -14,6 +14,11 @@ class Migration(migrations.Migration):
             name='Order',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('user', models.ForeignKey(on_delete=models.CASCADE, to='authentication.CustomUser')),  # Зв'язок з користувачем
+                ('book', models.ForeignKey(on_delete=models.CASCADE, to='book.Book')),  # Зв'язок з книгою
+                ('created_at', models.DateTimeField(auto_now_add=True)),  # Дата створення замовлення
+                ('plated_end_at', models.DateTimeField()),  # Запланована дата завершення
+                ('end_at', models.DateTimeField(null=True, blank=True)),  # Дата завершення
             ],
         ),
     ]
