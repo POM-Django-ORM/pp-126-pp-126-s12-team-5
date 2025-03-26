25b2cd9420ba7b7ffe5879986ce2b689de230a0f
sanya1213 <165886127+sanya1213@users.noreply.github.com>
2025-03-26 09:19:27 +0200

    Update admin.py

--- library/authentication/admin.py
+++ library/authentication/admin.py
@@ -1,1 +1,9 @@
 # Register your models here.
+from django.contrib import admin
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'role', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('role', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
